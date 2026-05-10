03ef36cc3d51ac48e2f53a385789637ecf1649d5
Matteo Calcagni <matteo.calcagni03@gmail.com>
2026-05-10 11:02:56 +0200

    Completed the model, with also training and testing functions

--- Siloi_report/report_fcnn.py
+++ Siloi_report/report_fcnn.py
@@ -1,5 +1,3 @@
-from calendar import day_abbr
-
 import numpy as np
 import pandas as pd
 import torch 
@@ -7,6 +5,12 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader, random_split
 
+
+########################################################################
+# CUSTOM FUNCTIONS NEEDED FOR IN MAIN CODE
+########################################################################
+
+# Load the dataset and clean it from unwanted data types
 def data_load_clean(data_file):
     
     # Load all data
@@ -24,6 +28,48 @@ def data_load_clean(data_file):
 
     return data
 
+# Train the model
+def train_model(teenmodel, num_epochs, train_dataloader):
+    
+    # Set the model in training 
+    teenmodel.train()
+
+    for epoch in range(num_epochs):
+        for i, (samples, labels) in enumerate(train_dataloader):
+
+            # forward pass
+            outputs = teenmodel(samples)
+            loss = criterion(outputs, labels)
+
+            # backpropagation
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        
+        if (epoch + 1) % 10 == 0:
+            print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
+
+# Test the model
+def test_model(teenmodel, test_dataloader):
+
+    teenmodel.eval()
+
+    with torch.no_grad():
+        n_correct = 0 
+        n_samples = 0
+        for samples, labels in test_dataloader:
+            outputs = teenmodel(samples) 
+            predictions = torch.argmax(outputs, dim=1)
+            n_correct += (predictions == labels).sum().item()
+            n_samples += labels.shape[0]
+
+    accuracy = 100.0 * (n_correct / n_samples)
+    print(f'The current model accuracy is: {accuracy} %')
+
+     
+########################################################################
+# DATASET AND NEURAL NETWORK CLASSES
+########################################################################
 
 class ReportDataset(Dataset):
 
@@ -42,7 +88,7 @@ class ReportDataset(Dataset):
 
         # I divide samples from labels and transform everything to torch tensors
         self.samples = torch.tensor(data[features].values, dtype=torch.float32) # load features samples
-        self.labels = torch.tensor(data[labels].values, dtype=torch.float32) # load the labels
+        self.labels = torch.tensor(data[labels].values, dtype=torch.long) # load the labels
         self.transform = transform # Needed transformations of both samples and labels
 
     def __len__(self):
@@ -59,14 +105,35 @@ class ReportDataset(Dataset):
 
         return sample, label # Return the sample and the label corresponding to the index
 
-class TeenNet(nn.Module):
+class EX1Net(nn.Module):
     
-    def __init__(self):
-        super(TeenNet, self).__init__()
+    def __init__(self, input_size, num_classes, p_dropout):
+        super(EX1Net, self).__init__()
+        self.fc1 = nn.Linear(input_size, 20)    # First layer - Input
+        self.fc2 = nn.Linear(20, 20)            # Second layer - hidden
+        self.fc3 = nn.Linear(20, num_classes)   # Third layer - output
+        self.dropout = nn.Dropout(p_dropout)
 
     def forward(self, x):
-        pass
+        
+        # First layer pass
+        out = self.fc1(x) 
+        out = F.relu(out)
+        out = self.dropout(out)
 
+        # Second layer pass
+        out = self.fc2(out)
+        out = F.relu(out)
+        out = self.dropout(out)
+
+        # Third layer pass
+        out = self.fc3(out)
+
+        return out
+
+########################################################################
+# MAIN CODE
+########################################################################
 
 # Data file path
 datafile = r'Siloi_report/Teen_Mental_Health_Dataset.csv'
@@ -76,6 +143,8 @@ features = ["age","gender","daily_social_media_hours","platform_usage","sleep_ho
             "screen_time_before_sleep","academic_performance",
             "physical_activity","social_interaction_level"]
 labels = "depression_label"
+
+num_features, num_classes = len(features), 10
 
 # Define the dataset
 teendataset = ReportDataset(datafile, features, labels, None)
@@ -94,3 +163,22 @@ training_set, test_set = random_split(teendataset, [training_samples, test_sampl
 # Defines the dataloader for both training and test sets
 train_dataloader = DataLoader(dataset=training_set, batch_size=40, shuffle=True)
 test_dataloader = DataLoader(dataset=test_set, batch_size=40, shuffle=False)
+
+########################################################################
+
+# Model definition and setting hyperparameters
+
+p_drop, learning_rate, num_epochs = 0.2, 0.01, 100
+
+teenmodel = EX1Net(num_features, num_classes, p_drop)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(teenmodel.parameters(), lr=learning_rate)
+
+# Training phase
+
+train_model(teenmodel, num_epochs, train_dataloader)
+
+# Test phase
+
+test_model(teenmodel, test_dataloader)
